a07a3058345a0a66d5610293771cd5e7ef24dddb
immber <jessiekrushing@gmail.com>
2025-08-15 20:00:49 -0700

    fix rss feed, hook it to leaflet posts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,20 @@
 import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
+import createSlug from "../lib/createSlug";
+
 
 export async function GET(context) {
-  const blog = await getCollection("blog");
+  const blog = await getCollection("leaflet");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: "coastweb.dev",
+    description: "MCP for curious developers",
     site: import.meta.env.SITE,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/blog/${post.slug}/`,
+      link: `/leaflet/${createSlug(post.data.title)}/`,
     })),
   });
 }
